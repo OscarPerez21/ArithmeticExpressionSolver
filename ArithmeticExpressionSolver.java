d2378dbee4cdec57b607734c23b57011f1cf0bc5
OscarPerez21 <133513764+OscarPerez21@users.noreply.github.com>
2023-08-17 12:48:10 -0500

    Update ArithmeticExpressionSolver.java

--- ArithmeticExpressionSolver.java
+++ ArithmeticExpressionSolver.java
@@ -1,127 +1,204 @@
-public class ArithmeticExpressionSolver {
-    public String resultado= "";
-    public int numOperadores= 0, indice= 0;
+public class ArithmeticExpressionSolver{
 
-    public String resolve(String calculo) {
-        resultado= "";
-        int numClases= 0;
-        resultado= calculo;
+    private int operaciones1= 0, operaciones2= 0, operaciones3= 0, operaciones4= 0, maxNivel= 0, nivel= 0;;
+    private int indice= 0, pos1=0, pos2= 0;
 
-        if(operationValidate(calculo)){
-            String[] clases= calculo.split("([\\+\\-\\*\\/])");
-            numClases= clases.length;}
-        else{
-            resultado= "Error";}
-
-        String[] clases = resultado.split("([\\+\\-\\*\\/])");
-        String[] operadores= resultado.replaceAll("[^\\+\\-\\*\\/]", "").split("");
-        Float[] valClases= new Float[numClases];
-        numOperadores= operadores.length;
-        int operadoresTotales = operadores.length; 
-
-        int operaciones3= 0, operaciones4= 0;
-
-        for(int i=0; i<operadores.length; i++){
-            if(operadores[i].equals("*")||operadores[i].equals("/")){ operaciones3++; }
-            if(operadores[i].equals("+")||operadores[i].equals("-")){ operaciones4++; }
+public String resolve(String operacion){
+    operaciones1= 0;
+        //determinar cuántos niveles de parentesis hay
+        String[] parenth= operacion.replaceAll("[^\\(\\)]", "").split("");
+        for(int i=0; i<parenth.length; i++){
+            if(parenth[i].equals("(")){
+                nivel++;
+                if(nivel>maxNivel){ maxNivel= nivel; }
+            }
+            if(parenth[i].equals(")")){ nivel--; }
         }
 
-        for(int i=0; i<numClases; i++){ valClases[i]= Float.parseFloat(clases[i]);}
+        //determinar cuantos grupos de parentesis hay en cada nivel
+        int[] parenthPorNivel= new int[maxNivel];
+        for(int i=0; i<maxNivel; i++){ parenthPorNivel[i]= 0; }
 
-        if(!resultado.equals("Error")){
-            int numImpresiones= 0;
-            String[] listaOperadores= {"*","/","+","-"};
-            int posOperador= 0;
-            imprimir(valClases, operadores);
-
-            while(numOperadores>0){
-                if(operadores[indice].equals("n")){ break; }
-                if(operadores[indice].equals(listaOperadores[posOperador]) || operadores[indice].equals(listaOperadores[posOperador+1])){ 
-                    if(posOperador==0){ operaciones3--; }
-                    if(posOperador==2){ operaciones4--; }  
-
-                    operaciones(indice, operadores, valClases, operadores[indice]);
-                    indice= -1;
-                }
-                if(operaciones3==0){ posOperador= 2; }
-                indice++;
+        for(int i=0; i<parenth.length; i++){
+            if(parenth[i].equals("(")){
+                nivel++;
+                parenthPorNivel[nivel-1]++;
             }
+            if(parenth[i].equals(")")){ nivel--; }
+        }
 
-            System.out.println(" "+valClases[0]);
-            if(resultado.length()!=1){
-                String tempResult= invertirCadena(resultado); resultado= "";
-                if(tempResult.charAt(1)=='.' && tempResult.charAt(0)=='0'){
-                    for (int i= 0; i<tempResult.length()-2; i++){ resultado+= tempResult.charAt(i+2); }
+        if(maxNivel>0){
+            System.out.println(operacion);
+            while(true){
+                if(operacion.charAt(operaciones1)=='('){ nivel++; } 
+                if(operacion.charAt(operaciones1)==')'){ nivel--; }
+                if(nivel==maxNivel && parenthPorNivel[maxNivel-1]>0){
+                    parenthPorNivel[maxNivel-1]--;
+                    operacion= resolverClase(operacion);
+                    System.out.println("operacion: "+operacion);
+                    operaciones1--;
                 }
-                else{ resultado= tempResult; }
-                if(resultado.equals("ytinifnI")){ resultado= "División entre 0"; }
-                else{ resultado= invertirCadena(resultado); }
+                operaciones1++;
+                if(parenthPorNivel[maxNivel-1]<=0){ maxNivel--; operaciones1= 0; nivel= 0; System.out.println();}
+                if(maxNivel==0){ break; }
             }
+        }
+        return(resolveOperation(operacion));
+    }
+
+    public String resolverClase(String operacion){
+        String temp= "", resultado= ""; int posParenth= operaciones1;
+        if(operacion.charAt(operaciones1)=='('){
+            while(true){
+                if(operacion.charAt(operaciones1)=='('){ operacion= deleteChar(operacion, operaciones1); }
+                if(operacion.charAt(operaciones1)==')'){ operacion= deleteChar(operacion, operaciones1); operaciones1-=2; nivel--; break; }
+                temp+= operacion.charAt(operaciones1);
+                operaciones1++;
+            }
+        }
+
+        for(int i=0; i<operacion.length(); i++){
+            if(i==posParenth){ resultado+= resolveOperation(temp); i+= temp.length()-1; }
+            else{ resultado+= operacion.charAt(i); }
+        }
+
+        operacion= resultado;
+        return operacion;
+    }
+
+    public String resolveOperation(String cadena){
+        operaciones2= 0; operaciones3= 0; operaciones4= 0;
+        //determinar numero de operadores
+        for(int i=0; i<cadena.length(); i++){
+            if(cadena.charAt(i)=='*'||cadena.charAt(i)=='/'){ operaciones3++; }
+            if(cadena.charAt(i)=='^'){ operaciones2++; }
+        }
+
+        //resolver operaciones individuales
+       //System.out.println(cadena);
+        cadena= resolverOrden(cadena, operaciones2, '^', '^');
+        cadena= resolverOrden(cadena, operaciones3, '*', '/');
+        for(int i=0; i<cadena.length(); i++){
+            if(cadena.charAt(i)=='+'||cadena.charAt(i)=='-'){ operaciones4++; }
+        }
+        if(operaciones4== 1 && cadena.charAt(0)=='-'){ operaciones4--; }
+        cadena= resolverOrden(cadena, operaciones4, '+', '-');
+
+        return cadena;
+    }
+
+    // funciones extra //
+
+    private String resolverOrden(String cadena, int numOperadores, char c1, char c2){
+        String resultado= "";
+        Float valorA= 0f, valorB= 0f;
+        System.out.println(cadena);
+        while(true){
+            String tempString= "";
+            if(cadena.charAt(indice)==c1||cadena.charAt(indice)==c2 && cadena.charAt(0)!='-'){
+                valorA= foundValorA(cadena);
+                valorB= foundValorB(cadena);
+                String str= operar(cadena.charAt(indice), valorA, valorB);
+
+                if(str.charAt(0)!='-' && cadena.charAt(0)!='+'){ str= "+"+str;}
+                
+                cadena= insertInSpace(cadena, pos1, pos2, str);
+                if(cadena.charAt(0)=='+'){
+                    for(int i=1; i<cadena.length(); i++){
+                        if(cadena.charAt(i)=='+'&& cadena.charAt(i+1)=='+'){ i++;}
+                        tempString+= cadena.charAt(i);
+                    }
+                    cadena= tempString;
+                }
+                tempString= "";
+                for(int i=0; i<cadena.length(); i++){
+                    String signos= ""; signos+=cadena.charAt(i);
+                    if(i<cadena.length()-1){ signos+= cadena.charAt(i+1); }
+                    switch(signos){
+                        case "+-": tempString+= "-"; i++; break;
+                        case "-+": tempString+= "-"; i++; break;
+                        case "--": tempString+= "+"; i++; break;
+                        case "++": tempString+= "+"; i++; break;
+                        case "/+": tempString+= "/"; i++; break;
+                        case "*+": tempString+= "*"; i++; break;
+                        case "^+": tempString+= "^"; i++; break;
+                        default: tempString+= cadena.charAt(i);
+                    }      
+                }
+                cadena= tempString;
+
+                numOperadores--;
+                System.out.println(cadena);
+            }
+            if(numOperadores==0){ indice= 0; break; }
+            indice++;
+        }
+        return cadena;
+    }
+
+    private String operar(char operador, Float valorA, Float valorB){
+        Float resultado= valorA;
+        String regresar= "";
+        switch(operador){
+            case '*': resultado= valorA*valorB; break;
+            case '/': resultado= valorA/valorB; break;
+            case '+': resultado= valorA+valorB; break;
+            case '-': resultado= valorA-valorB; break;
+            case '^': for(int i=0; i<valorB; i++){ resultado*= valorA; break; }
+        }
+        regresar= Float.toString(resultado);
+        if(regresar.charAt(regresar.length()-1)=='0' && regresar.charAt(regresar.length()-2)=='.'){
+            String tempString= "";
+            for(int i=0; i<regresar.length()-2; i++){ tempString+= regresar.charAt(i); }
+            regresar= tempString;
+        }
+
+        indice= -1;
+        return regresar;
+    }
+
+    /*Elimina una seccion determinada y la reemplaza por un texto*/
+    private String insertInSpace(String texto, int pos1, int pos2, String text){
+        String resultado= "";
+        for(int i=0; i<texto.length(); i++){
+            if(i==pos1){ resultado+= text; i+= (pos2-pos1); }
+            else{ resultado+= texto.charAt(i); }
         }
         return resultado;
     }
 
-    //Resolver orden de operaciones
-
-    private void operaciones(int i, String[] operadores, Float[] valClases, String operador){
-        switch(operador){
-            case "*": valClases[i]= valClases[i]*valClases[i+1]; break;
-            case "/": valClases[i]= valClases[i]/valClases[i+1]; break;
-            case "+": valClases[i]= valClases[i]+valClases[i+1]; break;
-            case "-": valClases[i]= valClases[i]-valClases[i+1]; break;
-        }
-        operadores[i]= "n"; numOperadores--;
-        valClases= bubbleSortLastFloat((i+1), valClases, valClases[i+1]);
-        operadores= bubbleSortLastString(operadores, "n");
-        resultado= Float.toString(valClases[i]);
-        imprimir(valClases, operadores);
-    }
-
-    private void imprimir(Float[] str, String[] op){
-        for(int i=0; i<numOperadores; i++){
-            System.out.print(" "+str[i]+" "+op[i]);
-            if(i==numOperadores-1){
-                System.out.print(" "+str[i+1]+" ");
+    private float foundValorA(String cadena){
+        String temp= ""; float resultado= 0;
+        int i= indice-1;
+        while(true){
+            if(cadena.charAt(indice)=='^'){
+                if(i<0||cadena.charAt(i)=='-'){ break; }
             }
+            if(i<0||cadena.charAt(i)=='*'||cadena.charAt(i)=='/'||cadena.charAt(i)=='+'||cadena.charAt(i)=='^'){ break; }
+            if(cadena.charAt(i+1)=='-' && i+1!=indice){ break; }
+            temp+= cadena.charAt(i);
+            pos1= i;
+            i--;
         }
-        if(numOperadores!=0){ System.out.println(); }
+        resultado= Float.parseFloat(invertirCadena(temp));
+        return resultado;
     }
 
-    //validar operación
-    private Boolean operationValidate(String str){
-        Boolean result= true;
-        for(int i=0; i<str.length(); i++){
-            if(i!=0){ 
-                if(isOperator(str.charAt(i))&&isOperator(str.charAt(i-1))){
-                    System.out.println("Error: no pueden haber dos operadores seguidos");
-                    result= false;}
-            }
+    private float foundValorB(String cadena){
+        String temp= ""; float resultado= 0;
+        int i= indice+1;
+        while(true){
+            if(i>=cadena.length()||cadena.charAt(i)=='*'||cadena.charAt(i)=='/'||cadena.charAt(i)=='+'){ break; }
+            if(cadena.charAt(i)=='-' && cadena.charAt(i-1)!='/' && cadena.charAt(i-1)!='*'){ break; }
+            temp+= cadena.charAt(i);
+            pos2= i;
+            i++;
         }
-        if(isOperator(str.charAt(0))){
-            resultado= invertirCadena(resultado);
-            resultado+= "0";
-            resultado= invertirCadena(resultado);
-        }
-
-        if(isOperator(str.charAt(str.length()-1))){
-            String tempStr= "";
-            for(int i=0; i< str.length()-1; i++){ tempStr+= str.charAt(i); }
-            resultado= tempStr;
-        }
-
-        return result;
+        resultado= Float.parseFloat(temp);
+        return resultado;
     }
 
-    private Boolean isOperator(Character list){
-        Character[] onList= {'+','-','*','/'};
-        Boolean result= false;
-        for(Character caracter : onList){
-            if(list.equals(caracter)){ result= true; }
-        }
-        return result;
-    }
-
-    //funciones extra / locales
+    /*-----------------*/
     private static String invertirCadena(String cadena) {
         String cadenaInvertida = "";
         for (int i = cadena.length() - 1; i >= 0; i--) {
@@ -130,38 +207,11 @@ public class ArithmeticExpressionSolver {
         return cadenaInvertida;
     }
 
-    private Float[] bubbleSortLastFloat(int indice, Float[] str, Float VALOR) {
-        int n = str.length;
-        int lastUsedIndex = 0;
-        int[] pos= new int[n];
-
-        for(int i=0; i<n-1; i++){ pos[i]= i; }
-        for (int i = 0; i < n - 1; i++) {
-            if (str[i].equals(VALOR) && pos[i]==indice) {
-                Float temp = str[i];
-                str[i] = str[i + 1];
-                str[i + 1] = temp;
-
-                int temp2= pos[i];
-                pos[i]= pos[i+1];
-                pos[i+1] = temp2;
-            }
+    public String deleteChar(String texto, int pos){
+        String resultado= "";
+        for(int i=0; i<texto.length(); i++){
+            if(!(i==pos)){ resultado+= texto.charAt(i); }
         }
-        return str;
+        return resultado;
     }
-
-    private String[] bubbleSortLastString(String[] str, String VALOR){
-        int n = str.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (str[j].equals(VALOR)) {
-                    String temp = str[j];
-                    str[j] = str[j + 1];
-                    str[j + 1] = temp;
-                }
-            }
-        }
-        return str;
-    }
-
 }
